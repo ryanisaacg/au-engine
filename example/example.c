8fa1a95f5f119bc360e1fc8277c81ce75b19d7a8
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-01 18:04:56 -0400

    Added a very simple draw to the example to test the current functions

--- example/example.c
+++ example/example.c
@@ -6,6 +6,26 @@
 int main()
 {
 	AU_Context *ctx = AU_Init(800, 600);
-	SDL_Delay(10);
+	GPU_Image* img = GPU_LoadImage("img.bmp");
+
+	int texid = AU_RegisterTexture(ctx, img);
+
+	AU_Clear(ctx);
+	int tl = AU_AddVertex(ctx, texid, 0, 0, 0, 0, 1, 1, 1, 1);
+	int tr = AU_AddVertex(ctx, texid, 32, 0, 1, 0, 1, 1, 1, 1);
+	int br = AU_AddVertex(ctx, texid, 32, 32, 1, 1, 1, 1, 1, 1);
+	int bl = AU_AddVertex(ctx, texid, 0, 32, 0, 1, 1, 1, 1, 1);
+	
+	AU_AddIndex(ctx, texid, tl);
+	AU_AddIndex(ctx, texid, tr);
+	AU_AddIndex(ctx, texid, br);
+
+	AU_AddIndex(ctx, texid, tr);
+	AU_AddIndex(ctx, texid, br);
+	AU_AddIndex(ctx, texid, bl);
+
+	AU_Present(ctx);
+
+	SDL_Delay(1000);
 	AU_Quit(ctx);
 }
