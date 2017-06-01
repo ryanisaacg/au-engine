7507c04e3d0b9107fc7b62c80a7f9cc957dfad6a
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-01 18:12:26 -0400

    Showed with the example the drawing pipeline works

--- example/example.c
+++ example/example.c
@@ -3,18 +3,11 @@
 
 #undef main
 
-int main()
-{
-	AU_Context *ctx = AU_Init(800, 600);
-	GPU_Image* img = GPU_LoadImage("img.png");
-
-	int texid = AU_RegisterTexture(ctx, img);
-
-	AU_Clear(ctx);
-	int tl = AU_AddVertex(ctx, texid, 0, 0, 0, 0, 1, 1, 1, 1);
-	int tr = AU_AddVertex(ctx, texid, 32, 0, 1, 0, 1, 1, 1, 1);
-	int br = AU_AddVertex(ctx, texid, 32, 32, 1, 1, 1, 1, 1, 1);
-	int bl = AU_AddVertex(ctx, texid, 0, 32, 0, 1, 1, 1, 1, 1);
+void draw(AU_Context *ctx, int texid, float xOffset) {
+	int tl = AU_AddVertex(ctx, texid, xOffset, 0, 0, 0, 1, 1, 1, 1);
+	int tr = AU_AddVertex(ctx, texid, xOffset + 32, 0, 1, 0, 1, 1, 1, 1);
+	int br = AU_AddVertex(ctx, texid, xOffset + 32, 32, 1, 1, 1, 1, 1, 1);
+	int bl = AU_AddVertex(ctx, texid, xOffset, 32, 0, 1, 1, 1, 1, 1);
 	
 	AU_AddIndex(ctx, texid, tl);
 	AU_AddIndex(ctx, texid, tr);
@@ -23,7 +16,22 @@ int main()
 	AU_AddIndex(ctx, texid, br);
 	AU_AddIndex(ctx, texid, bl);
 	AU_AddIndex(ctx, texid, tl);
+}
 
+int main()
+{
+	AU_Context *ctx = AU_Init(800, 600);
+	GPU_Image* img = GPU_LoadImage("img.png");
+	GPU_Image* img2 = GPU_LoadImage("img2.png");
+
+	int texid1 = AU_RegisterTexture(ctx, img);
+	int texid2 = AU_RegisterTexture(ctx, img2);
+
+	AU_Clear(ctx);
+	draw(ctx, texid1, 0);
+	draw(ctx, texid2, 32);
+	draw(ctx, texid1, 64);
+	draw(ctx, texid2, 96);
 	AU_Present(ctx);
 
 	SDL_Delay(1000);
