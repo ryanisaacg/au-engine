e09be1df6369ef25b6b0f0202269621b1171349a
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-02 19:03:02 -0400

    Created a texture struct and added a function to load it

--- window.c
+++ window.c
@@ -1,9 +1,22 @@
 #include "window.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "memory.h"
 
 AU_Engine *AU_Init(char* title, int w, int h) {
 	AU_Engine* engine = chk_alloc(sizeof(AU_Engine));
 	engine->ctx = AU_Context_Init_Stack(title, w, h);
 	return engine;
+}
+
+AU_Texture AU_LoadTexture(AU_Engine* eng, char* name) {
+	GPU_Image* image = GPU_LoadImage(name);
+	if(image == NULL) {
+		fprintf(stderr, "Failed to load image with filename %s", name);
+		exit(1);
+	}
+	int id = AU_Context_RegisterTexture(&(eng->ctx), image);
+	return (AU_Texture) { id, image->w, image->h };
 }
--- window.h
+++ window.h
@@ -3,11 +3,16 @@
 #include "context.h"
 
 typedef struct {
+	int id, width, height;
+} AU_Texture;
+
+typedef struct {
 	AU_Context ctx;
 } AU_Engine;
 
 //Intialize the engine with a window title, width, and height
 AU_Engine *AU_Init(char*, int, int);
-
-
-
+//Load a texture from a file with a given name
+AU_Texture AU_LoadTexture(AU_Engine*, char* name);
+//Draw a texture at a rectangular position
+void AU_DrawTextureRect(AU_Engine*, AU_Texture, 
