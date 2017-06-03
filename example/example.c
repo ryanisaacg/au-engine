8869f5162a04b067ba62ee9e9c22d248dd5a740a
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-02 21:51:32 -0400

    Moved the example images into source control

--- example/example.c
+++ example/example.c
@@ -4,8 +4,8 @@
 int main()
 {
 	AU_Engine *eng = au_init("TEST WINDOW", 800, 600);
-	AU_Texture img = au_load_texture(eng, "img.png");
-	AU_Texture img2 = au_load_texture(eng, "img2.png");
+	AU_Texture img = au_load_texture(eng, "../example/img.png");
+	AU_Texture img2 = au_load_texture(eng, "../example/img2.png");
 	while(eng->should_continue) {
 		au_begin(eng);
 		au_draw_texture(eng, img, 0, 0, 32, 32);
