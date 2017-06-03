79ab0ef84a6e72914d46398543eabb4909def290
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-02 21:50:46 -0400

    Fixed the geometry implementation intialization of matrices

--- geom.c
+++ geom.c
@@ -3,20 +3,19 @@
 #include <math.h>
 
 AU_Transform au_geom_identity() {
-	float data[][] = {
+	return (AU_Transform) {{
 		{1, 0, 0},
 		{0, 1, 0},
-		{0, 0, 1}}
-	return (AU_Transform) { data };
+		{0, 0, 1}}};
 }
 
 AU_Vector au_geom_transform(AU_Transform matrix, AU_Vector vector) {
-	float data[] = {vector.x, vector.y, 1};
+	float data[3] = {vector.x, vector.y, 1};
 	float result[3];
 	for(int i = 0; i < 3; i++) {
-		result[j] = 0;
+		result[i] = 0;
 		for(int j = 0; j < 3; j++) {
-			result[j] += matrix.data[j] * data[j];
+			result[i] += matrix.data[i][j] * data[j];
 		}
 	}
 	return (AU_Vector) { result[0], result[1] };
@@ -33,27 +32,24 @@ AU_Transform au_geom_trasnform_concat(AU_Transform a, AU_Transform b) {
 }
 
 AU_Transform au_geom_transform_translate(float x, float y) {
-	float data[][] = {
+	return (AU_Transform) {{
 		{1, 0, x},
 		{0, 1, y},
-		{0, 0, 1}}
-	return (AU_Transform) { data };
+		{0, 0, 1}}};
 }
 
 AU_Transform au_geom_transform_rotate(float angle) {
-	float c = cos(angle * 180f / M_PI);
-	float s = sin(angle * 180f / M_PI);
-	float data[][] = {
+	float c = cos(angle * 180.0f / M_PI);
+	float s = sin(angle * 180.0f / M_PI);
+	return (AU_Transform) {{
 		{c, -s, 0},
 		{s, c, 0},
-		{0, 0, 1}}
-	return (AU_Transform) { data };
+		{0, 0, 1}}};
 }
 
 AU_Transform au_geom_transform_scale(float x_scale, float y_scale) {
-	float data[][] = {
-		{x, 0, 0},
-		{0, y, 0},
-		{0, 0, 1}}
-	return (AU_Transform) { data };
+	return (AU_Transform) {{
+		{x_scale, 0, 0},
+		{0, y_scale, 0},
+		{0, 0, 1}}};
 }
