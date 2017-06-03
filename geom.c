eec2d49e836ba30e835dcd211dd45605bbbd038f
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-02 21:25:00 -0400

    Added functions to handle creating various transformations

--- geom.c
+++ geom.c
@@ -1,5 +1,7 @@
 #include "geom.h"
 
+#include <math.h>
+
 AU_Transform au_geom_identity() {
 	float data[][] = {
 		{1, 0, 0},
@@ -18,4 +20,40 @@ AU_Vector au_geom_transform(AU_Transform matrix, AU_Vector vector) {
 		}
 	}
 	return (AU_Vector) { result[0], result[1] };
+}
+
+AU_Transform au_geom_trasnform_concat(AU_Transform a, AU_Transform b) {
+	AU_Transform c = a;
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < 3; j++) {
+			c.data[i][j] = a.data[i][j] * b.data[j][i];
+		}
+	}
+	return c;
+}
+
+AU_Transform au_geom_transform_translate(float x, float y) {
+	float data[][] = {
+		{1, 0, x},
+		{0, 1, y},
+		{0, 0, 1}}
+	return (AU_Transform) { data };
+}
+
+AU_Transform au_geom_transform_rotate(float angle) {
+	float c = cos(angle * 180f / M_PI);
+	float s = sin(angle * 180f / M_PI);
+	float data[][] = {
+		{c, -s, 0},
+		{s, c, 0},
+		{0, 0, 1}}
+	return (AU_Transform) { data };
+}
+
+AU_Transform au_geom_transform_scale(float x_scale, float y_scale) {
+	float data[][] = {
+		{x, 0, 0},
+		{0, y, 0},
+		{0, 0, 1}}
+	return (AU_Transform) { data };
 }
--- geom.h
+++ geom.h
@@ -14,3 +14,11 @@ typedef struct {
 AU_Transform au_geom_identity();
 //Apply a transform matrix to a vector
 AU_Vector au_geom_transform(AU_Transform, AU_Vector);
+//Concatenate two transformations together
+AU_Transform au_geom_trasnform_concat(AU_Transform, AU_Transform);
+//Get a translation matrix
+AU_Transform au_geom_transform_translate(float x, float y);
+//Get a rotation matrix (in degrees)
+AU_Transform au_geom_transform_rotate(float angle);
+//Get a scale matrix
+AU_Transform au_geom_transform_scale(float x_scale, float y_scale);
