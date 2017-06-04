6520765abc9baec472a5d1095f261d094cfab04a
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-04 16:21:24 -0400

    Added geometry structures and collision functions

--- geom.c
+++ geom.c
@@ -2,6 +2,27 @@
 
 #include <math.h>
 
+AU_Vector au_geom_scl(AU_Vector vec, float scl) {
+	return (AU_Vector) {
+		vec.x* scl, vec.y* scl
+	};
+}
+
+AU_Vector au_geom_nor(AU_Vector vec) {
+	float len = au_geom_len(vec);
+	return (AU_Vector) {
+		vec.x / len, vec.y / len
+	};
+}
+
+float au_geom_len2(AU_Vector vec) {
+	return vec.x * vec.x + vec.y * vec.y;
+}
+
+float au_geom_len(AU_Vector vec) {
+	return sqrt(au_geom_len2(vec));
+}
+
 AU_Transform au_geom_identity() {
 	return (AU_Transform) {
 		{
@@ -66,4 +87,44 @@ AU_Transform au_geom_transform_scale(float x_scale, float y_scale) {
 			{0, 0, 1}
 		}
 	};
+}
+
+bool au_geom_rect_overlaps_rect(AU_Rectangle a, AU_Rectangle b) {
+	return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
+}
+
+bool au_geom_circ_overlaps_circ(AU_Circle a, AU_Circle b) {
+	float x = a.x - b.x;
+	float y = a.y - b.y;
+	return x * x + y * y < a.radius * b.radius;
+}
+
+bool au_geom_rect_overlaps_circ(AU_Rectangle r, AU_Circle c) {
+	AU_Vector closest;
+	if (c.x < r.x) {
+		closest.x = r.x;
+	} else if (c.x > r.x + r.width) {
+		closest.x = r.x + r.width;
+	} else {
+		closest.x = c.x;
+	}
+	if (c.y < r.y) {
+		closest.y = r.y;
+	} else if (c.y > r.y + r.height) {
+		closest.y = r.y + r.height;
+	} else {
+		closest.y = c.y;
+	}
+	closest.x -= c.x;
+	closest.y -= c.y;
+	return (closest.x * closest.x) + (closest.y * closest.y) < c.radius * c.radius;
+}
+
+bool au_geom_rect_contains(AU_Rectangle r, AU_Vector v) {
+	return v.x >= r.x && v.y >= r.y && v.x < r.x + r.width && v.y < r.y + r.height;
+}
+
+bool au_geom_circ_contains(AU_Circle c, AU_Vector v) {
+	AU_Vector dist = { v.x - c.x, v.y - c.y };
+	return au_geom_len2(dist) < c.radius * c.radius;
 }
--- geom.h
+++ geom.h
@@ -1,14 +1,26 @@
 #pragma once
 
-//A 3x3 matrix indexed by [row][col]
-typedef struct {
-	float data[3][3];
-} AU_Transform;
+#include <stdbool.h>
 
 //A 2D vector
 typedef struct {
 	float x, y;
 } AU_Vector;
+
+//Scale a vector by a scalar
+AU_Vector au_geom_scl(AU_Vector, float);
+//Normalize a vector to a length of one
+AU_Vector au_geom_nor(AU_Vector);
+//Get the squared length of a vector
+float au_geom_len2(AU_Vector);
+//Get the length of a vector
+float au_geom_len(AU_Vector);
+
+
+//A 3x3 matrix indexed by [row][col]
+typedef struct {
+	float data[3][3];
+} AU_Transform;
 
 //Return an identity transformation matrix
 AU_Transform au_geom_identity();
@@ -22,3 +34,20 @@ AU_Transform au_geom_transform_translate(float x, float y);
 AU_Transform au_geom_transform_rotate(float angle);
 //Get a scale matrix
 AU_Transform au_geom_transform_scale(float x_scale, float y_scale);
+
+//An axis aligned rectangle
+typedef struct {
+	float x, y, width, height;
+} AU_Rectangle;
+
+//A circle
+typedef struct {
+	float x, y, radius;
+} AU_Circle;
+
+bool au_geom_rect_overlaps_rect(AU_Rectangle, AU_Rectangle);
+bool au_geom_rect_overlaps_circ(AU_Rectangle, AU_Circle);
+bool au_geom_circ_overlaps_circ(AU_Circle, AU_Circle);
+#define au_geom_circ_overlaps_rect(circ, rect) (au_geom_rect_overlaps_circ(rect, circ))
+bool au_geom_rect_contains(AU_Rectangle, AU_Vector);
+bool au_geom_circ_contains(AU_Circle, AU_Vector);
