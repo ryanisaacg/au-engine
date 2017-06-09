cbdc36bdb47b6e162924757c855b1c9b9fa1577f
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-09 14:02:49 -0400

    Added a color struct that stores RGBA

--- tex.h
+++ tex.h
@@ -11,5 +11,9 @@ typedef struct {
 	AU_Rectangle rect;
 } AU_TextureRegion;
 
+typedef struct {
+	float r, g, b, a;
+} AU_Color;
+
 //Turns the entire texture into a texture region
 AU_TextureRegion au_tex_region(AU_Texture);
