191f0bcb0b7bf18d78fb7ddfe6e7766ebb66226d
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-09 21:33:25 -0400

    Created data structures to hold rendered text

--- text.h
+++ text.h
@@ -1,3 +1,15 @@
 #pragma once
 
-unsigned long au_text_hash(const char *str);
+#include <SDL2/SDL.h>
+
+typedef struct {
+	const char* text;
+	SDL_Surface surface;
+} AU_TextRenderEntry;
+
+typedef struct {
+	AU_TextRenderEntry* entries;
+	size_t entry_count, entry_capacity;
+} AU_TextCache;
+
+unsigned long au_text_hash(const char* str);
