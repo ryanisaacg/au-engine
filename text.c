fb0c66352db082f3ee710b6446c76c20b1157ddf
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-09 22:58:53 -0400

    Added functions to manage the string cache

--- text.c
+++ text.c
@@ -3,6 +3,8 @@
 #include <SDL2/SDL_ttf.h>
 #include <string.h>
 
+#include "memory.h"
+
 unsigned long au_text_hash(const char* str) {
 	unsigned long hash = 5381;
 	int len = strlen(str);
@@ -18,3 +20,42 @@ AU_TextRenderEntry au_text_render(TTF_Font* font, const char* text, AU_Color col
 	return (AU_TextRenderEntry) { text, rendered };
 }
 
+AU_TextCache* au_text_cache_init() {
+	AU_TextCache* cache = au_memory_alloc(sizeof(AU_TextCache));
+	cache->entry_count = 0;
+	cache->entry_capacity = 128;
+	cache->entries = au_memory_calloc(cache->entry_capacity, sizeof(AU_TextRenderEntry));
+	return cache;
+}
+
+static int probe_index(AU_TextCache* cache, const char* key) {
+	unsigned long hash = au_text_hash(key);
+	int index = hash % cache->entry_capacity;
+	while(cache->entries[index].text != NULL) 
+		index++;
+	return index;
+}
+
+void au_text_cache_add(AU_TextCache* cache, AU_TextRenderEntry entry) {
+	if(cache->entry_count * 2 < cache->entry_capacity) {
+		int old_capacity = cache->entry_capacity;
+		cache->entry_capacity *= 2;
+		AU_TextRenderEntry* prev = cache->entries;
+		cache->entries = au_memory_calloc(cache->entry_capacity, sizeof(AU_TextRenderEntry));
+		for(int i = 0; i < old_capacity; i++) {
+			if(prev[i].text != NULL) 
+				au_text_cache_add(cache, prev[i]);
+		}
+	}
+	int i = probe_index(cache, entry.text);
+	cache->entries[i] = entry;
+}
+
+SDL_Surface* au_text_cache_get(AU_TextCache* cache, const char* key) {
+	return cache->entries[probe_index(cache, key)].surface;
+}
+
+void au_text_cache_destroy(AU_TextCache* cache) {
+	free(cache->entries);
+	free(cache);
+}
--- text.h
+++ text.h
@@ -10,10 +10,15 @@ typedef struct {
 	SDL_Surface* surface;
 } AU_TextRenderEntry;
 
+unsigned long au_text_hash(const char* str);
+AU_TextRenderEntry au_text_render(TTF_Font* font, const char* text, AU_Color color);
+
 typedef struct {
 	AU_TextRenderEntry* entries;
 	size_t entry_count, entry_capacity;
 } AU_TextCache;
 
-unsigned long au_text_hash(const char* str);
-AU_TextRenderEntry au_text_render(TTF_Font* font, const char* text, AU_Color color);
+AU_TextCache* au_text_cache_init();
+void au_text_cache_add(AU_TextCache*, AU_TextRenderEntry);
+SDL_Surface* au_text_cache_get(AU_TextCache*, const char*);
+void au_text_cache_destroy(AU_TextCache*);
