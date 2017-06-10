f3c678c99b942e409d061415264e16459b0b56aa
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-09 21:24:36 -0400

    Added a text module with a string hash

---
+++ text.c
@@ -0,0 +1,11 @@
+#include "text.h"
+
+#include <string.h>
+
+unsigned long au_text_hash(const char *str) {
+	unsigned long hash = 5381;
+    int len = strlen(str);
+	for (int i = 0; i < len; i++)
+		hash = ((hash << 5) + hash) + i;
+	return hash;
+}
---
+++ text.h
@@ -0,0 +1,3 @@
+#pragma once
+
+unsigned long au_text_hash(const char *str);
