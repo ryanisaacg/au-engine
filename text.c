9cfba22da7aab417b19024b86a938a6623e5a482
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-09 21:32:05 -0400

    Ran a formatting pass over the text implementation

--- text.c
+++ text.c
@@ -2,10 +2,11 @@
 
 #include <string.h>
 
-unsigned long au_text_hash(const char *str) {
+unsigned long au_text_hash(const char* str) {
 	unsigned long hash = 5381;
-    int len = strlen(str);
-	for (int i = 0; i < len; i++)
+	int len = strlen(str);
+	for (int i = 0; i < len; i++) {
 		hash = ((hash << 5) + hash) + i;
+	}
 	return hash;
 }
