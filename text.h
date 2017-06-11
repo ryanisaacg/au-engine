4354ea8edece115ac600096213945c6220fa9bb7
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-11 07:17:53 -0400

    Added a function to get the size on screen of rendered text

--- text.h
+++ text.h
@@ -13,6 +13,11 @@ typedef struct {
 //Recursive dependency- must come after type declaration
 #include "au.h"
 
+//Create a font from a TTF file
 AU_Font* au_font_init(AU_Engine*, TTF_Font*, AU_Color);
+//Get the texture region corresponding to a character
 AU_TextureRegion au_font_get_char(const AU_Font*, char);
+//Get the size that the string will take when drawn
+AU_Rectangle au_font_get_size(const AU_Font*, const char*);
+//Destroy a font
 void au_font_destroy(AU_Font*);
