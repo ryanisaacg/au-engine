af09ca94b28a0944c73ca7a219ac025ee2ff1c67
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-12 17:48:06 -0400

    Added a sprite module to integrate textures with transforms

---
+++ sprite.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "animation.h"
+#include "texture.h"
+
+typedef struct {
+	float x, y, width, height, rotation, origin_x, origin_y, scale_x, scale_y;
+	bool flip_x, flip_y;
+	AU_Color color;
+} AU_SpriteTransform;
+
+AU_SpriteTransform au_sprite_transform_new();
+
+typedef struct {
+	AU_SpriteTransform transform;
+	AU_TextureRegion region;
+} AU_Sprite;
+
+AU_Sprite au_sprite_new(AU_TextureRegion);
+
+typedef struct {
+	AU_SpriteTransform transform;
+	AU_AnimationManager animations;
+} AU_AnimatedSprite;
+
+AU_AnimatedSprite au_sprite_anim_new(AU_AnimationManager);
+
