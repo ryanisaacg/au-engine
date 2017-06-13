178e60d4f987ad125751515c04ad610ef21e8210
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-13 09:40:32 -0400

    Added random functions to the util header

--- util.h
+++ util.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <math.h>
+#include <stdlib.h>
 
 static inline float au_util_clamp(float n, float lower, float upper) {
 	return n <= lower ? lower : n >= upper ? upper : n;
@@ -9,3 +10,15 @@ static inline float au_util_clamp(float n, float lower, float upper) {
 static inline bool au_util_floateq(float a, float b) {
 	return fabs(a - b) < 0.00001;
 }
+
+//A normalized random function
+static inline float au_util_rand() {
+	return (float) rand() / RAND_MAX;
+}
+static inline float au_util_randf_range(float min, float max) {
+	return (max - min) * au_util_rand() + min;
+}
+static inline int au_util_randi_range(int min, int max) {
+	return (int)au_util_randf_range(min, max);
+}
+
