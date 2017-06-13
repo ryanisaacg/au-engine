502c8175c7e2943d1068b71a673357bdaf847866
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-13 09:41:48 -0400

    Added functions for updating particles and emitters

---
+++ particle.c
@@ -0,0 +1,29 @@
+#include "particle.h"
+
+void au_particle_update(AU_Particle* part) {
+	part->velocity = au_geom_vec_add(part->velocity, part->acceleration);
+	part->position = au_geom_vec_add(part->position, part->velocity);
+	part->scale = au_geom_vec_add(part->scale, part->scale_velocity);
+	part->rotation += part->rotational_velocity;
+	part->lifetime--;
+}
+
+AU_ParticleEmitter au_particle_emitter_new(AU_TextureRegion *possibleTextures, size_t num_textures) {
+	return (AU_ParticleEmitter) {
+
+	};
+}
+
+AU_Particle au_particle_emit(AU_ParticleEmitter* emit) {
+	return (AU_Particle) { 
+		emit->regions[au_util_randi_range(0, emit->num_textures)], 
+		au_geom_vec_rand(emit->top_left, emit->bottom_right), 
+		au_geom_vec_rand(emit->velocity_min, emit->velocity_max),
+		au_geom_vec_rand(emit->acceleration_min, emit->acceleration_max),
+		au_geom_vec_rand(emit->scale_min, emit->scale_max),
+		au_geom_vec_rand(emit->scale_velocity_min, emit->scale_velocity_max),
+		au_util_randf_range(emit->rotation_min, emit->rotation_max),
+		au_util_randf_range(emit->rotational_velocity_min, emit->rotational_velocity_max),
+		au_util_randi_range(emit->lifetime_min, emit->lifetime_max)
+	};
+}
--- particle.h
+++ particle.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stddef.h>
+
 #include "texture.h"
 
 typedef struct {
@@ -9,9 +11,17 @@ typedef struct {
 	int lifetime;
 } AU_Particle;
 
+void au_particle_update(AU_Particle*);
+
 typedef struct {
+	AU_TextureRegion *regions;
+	size_t num_textures;
 	AU_Vector top_left, bottom_right, velocity_min, velocity_max, acceleration_min, acceleration_max, scale_min, scale_max,
-			  scale_velocity_min, scale_velocity_min;
+			  scale_velocity_min, scale_velocity_max;
 	float rotation_min, rotation_max, rotational_velocity_min, rotational_velocity_max;
 	int lifetime_min, lifetime_max;
+	int particle_min, particle_max;
 } AU_ParticleEmitter;
+
+AU_ParticleEmitter au_particle_emitter_new(AU_TextureRegion *possibleTextures, size_t num_textures);
+AU_Particle au_particle_emitter_emit(AU_ParticleEmitter*);
