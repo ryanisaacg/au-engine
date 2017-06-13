bec435349ab5b00806edb7e0855bf046f35ec163
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-13 15:37:51 -0400

    Set the default size of sprites to the size of the region it wraps

--- sprite.c
+++ sprite.c
@@ -6,14 +6,21 @@ AU_SpriteTransform au_sprite_transform_new() {
 	};
 }
 
+static AU_SpriteTransform au_sprite_transform_from_region(AU_TextureRegion region) {
+	AU_SpriteTransform trans = au_sprite_transform_new();
+	trans.width = region.rect.width;
+	trans.height = region.rect.height;
+	return trans;
+}
+
 AU_Sprite au_sprite_new(AU_TextureRegion region) {
 	return (AU_Sprite) {
-		au_sprite_transform_new(), region
+		au_sprite_transform_from_region(region), region
 	};
 }
 
 AU_AnimatedSprite au_sprite_anim_new(AU_AnimationManager manager) {
 	return (AU_AnimatedSprite) {
-		au_sprite_transform_new(), manager
+		au_sprite_transform_from_region(au_anim_manager_get_frame(&manager)), manager
 	};
 }
