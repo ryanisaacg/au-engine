6e486c2a9b79fa1ae6f0ee59ee6e498b9218f166
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-12 20:01:32 -0400

    Added a floating point equality function to the util

--- util.h
+++ util.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <math.h>
 
 static inline float au_util_clamp(float n, float lower, float upper) {
 	return n <= lower ? lower : n >= upper ? upper : n;
+}
+
+static inline bool au_util_floateq(float a, float b) {
+	return abs(a - b) < 0.00001;
 }
