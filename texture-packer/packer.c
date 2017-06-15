ebb93a459966065a4bd951ca82882d74d5e02c06
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-15 16:48:16 -0400

    Created an untested function to pack a series of images into a single texture

--- texture-packer/packer.c
+++ texture-packer/packer.c
@@ -1,3 +1,4 @@
+#include <SDL_gpu.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,7 +20,7 @@ int compare_rects(const void* ptr1, const void* ptr2) {
 
 //TODO: change the function to be in-place
 
-PackedRectangles pack_rects(AU_Rectangle* rects, size_t length) {
+AU_Rectangle pack_rects(AU_Rectangle* rects, size_t length) {
 	qsort(rects, length, sizeof(AU_Rectangle), &compare_rects);
 	float max_width = 0;
 	for(size_t i = 0; i < length; i++) {
@@ -27,7 +28,6 @@ PackedRectangles pack_rects(AU_Rectangle* rects, size_t length) {
 			max_width = rects[i].width;
 	}
 	max_width *= 4; //The goal is to be able to maximize level efficiency
-	AU_Rectangle* packed = au_memory_alloc(sizeof(AU_Rectangle) * length);
 	float current_width = 0, current_y = 0, current_height = rects[0].height;
 	for(size_t i = 0; i < length; i++) {
 		if(current_width + rects[i].width > max_width) {
@@ -35,23 +35,40 @@ PackedRectangles pack_rects(AU_Rectangle* rects, size_t length) {
 			current_y += current_height;
 			current_height = rects[i].height;
 		}
-		packed[i] = (AU_Rectangle) { current_width, current_y, rects[i].width, rects[i].height };
+		float old_width = current_width;
 		current_width += rects[i].width;	
+		rects[i] = (AU_Rectangle) { old_width, current_y, rects[i].width, rects[i].height };
 	}
-	return (PackedRectangles) { packed, length, (AU_Rectangle) { 0, 0, max_width, current_y + current_height } };
+	return (AU_Rectangle) { 0, 0, max_width, current_y + current_height };
+}
+
+void pack_to_file(char** filenames, size_t length, char* image_file, FILE* manifest) {
+	SDL_Surface** surfaces = au_memory_alloc(sizeof(SDL_Surface*) * length);
+	AU_Rectangle* rectangles = au_memory_alloc(sizeof(AU_Rectangle) * length);
+	for(size_t i = 0; i < length; i++) {
+		surfaces[i] = GPU_LoadSurface(filenames[i]);
+		rectangles[i] = (AU_Rectangle) { 0, 0, surfaces[i]->w, surfaces[i]->h };
+	}
+	AU_Rectangle total = pack_rects(rectangles, length);
+	SDL_Surface* full_surface =  SDL_CreateRGBSurface(0, total.width, total.height, 32, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
+	for(size_t i = 0; i < length; i++) {
+		SDL_Rect dest = {(int)rectangles[i].x, (int)rectangles[i].y, (int)rectangles[i].width, (int)rectangles[i].height };
+		SDL_BlitSurface(surfaces[i], NULL, full_surface, &dest);
+	}
+	GPU_SaveSurface(full_surface, image_file, GPU_FILE_AUTO);
+	for(size_t i = 0; i < length; i++) {
+		fprintf(manifest, "%f %f %f %f \n", rectangles[i].x, rectangles[i].y, rectangles[i].width, rectangles[i].height);
+		fprintf(manifest, "%s\n", filenames[i]);
+	}
 }
 
 void print_rect(AU_Rectangle r) {
 	printf("%f:%f:%f:%f\n", r.x, r.y, r.width, r.height);
 }
 
-int main() {
-	AU_Rectangle rectangles[] = {{0, 0, 100, 50}, {0, 0, 12, 12}, {0, 0, 32, 32}, {0, 0, 48, 48}, {9, 9, 32, 32}, {0, 0, 32, 32}, {0, 0, 32, 32}, {0, 0, 48, 48}, {0, 0, 128, 128}, {0, 0, 128, 4}, {0, 0, 16, 48}};
-	PackedRectangles packed = pack_rects(rectangles, sizeof(rectangles) / sizeof(AU_Rectangle));
-	printf("%f\n", packed.total_size.width * packed.total_size.height);
-	print_rect(packed.total_size);
-	for(size_t i = 0; i < packed.rectangle_count; i++) {
-		print_rect(packed.packed[i]);
-	}
+int main(int argc, char *argv[]) {
+	FILE* manifest = fopen(argv[1], "r");
+	pack_to_file(argv + 2, argc - 2, argv[0], manifest);
+	fclose(manifest);
 	return 0;
 }
