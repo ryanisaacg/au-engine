aa265d779c3bdd128479a19f07fdc7bcdb02fcd2
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-22 14:55:38 -0400

    Updated the example to reflect feature parity reached since before the backend rewrite

--- example/example.c
+++ example/example.c
@@ -1,4 +1,5 @@
 #include "au.h"
+#include <stdio.h>
 
 int main()
 {
@@ -9,21 +10,21 @@ int main()
 	while(eng->should_continue) {
 		au_begin(eng, AU_BLACK);
 		if(eng->current_keys[SDL_SCANCODE_UP])
-			y -= 2;
+			y -= 0.1;
 		if(eng->current_keys[SDL_SCANCODE_DOWN])
-			y += 2;
+			y += 0.1;
 		if(eng->current_keys[SDL_SCANCODE_RIGHT])
-			x += 2;
+			x += 0.1;
 		if(eng->current_keys[SDL_SCANCODE_LEFT])
-			x -= 2;
+			x -= 0.1;
 		if(eng->mouse_left) {
 			x = eng->mouse_x;
 			y = eng->mouse_y;
 		}
 		AU_TextureRegion region = au_tex_region(img);
 		region.rect = (AU_Rectangle) {8, 8, 16, 16};
-		au_draw_texture_ex(eng, region, AU_WHITE, 0, 0, 32, 32, 0, 0, 0, 1, 1, true, false, 0);
-		au_draw_texture_ex(eng, region, AU_WHITE, x, y, 32, 32, 0, 0, 0, 1, 1, true, false, -50);
+		au_draw_texture_ex(eng, region, AU_WHITE, 0, 0, 32, 32, 0, 0, 0, 1, 1, false, false, 0.5f);
+		au_draw_texture_ex(eng, region, AU_GREEN, x, y, 32, 32, 0, 0, 0, 1, 1, false, false, 0);
 		au_draw_string(eng, font, "Hello there! test string", 0, 100);
 		au_end(eng);
 	}
