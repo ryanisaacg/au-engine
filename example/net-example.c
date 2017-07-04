03ace54e6b05bfd2eb296d8d8c82a4ba22a05ccb
Ryan Goldstein <ryanisaacg@gmail.com>
2017-07-03 21:43:52 -0400

    Added a networking example to test the netcode

---
+++ example/net-example.c
@@ -0,0 +1,26 @@
+#include "au.h"
+
+#include <stdio.h>
+
+int main() {
+	au_init_headless();
+	char buffer[512];
+	printf("Run as server? [Y/n]: ");
+	fgets(buffer, 512, stdin);
+	//Run as server or client
+	if(buffer[0] == 'Y') {
+		AU_Socket server = au_socket_server_new(8888);
+		//AU_Socket client = au_socket_server_accept(server);
+		au_socket_udp_recv(server, buffer, 512);
+		printf("Message received: %s\n", buffer);
+	} else {
+		printf("Enter the address of the server to connect to:\n");
+		fgets(buffer, 512, stdin);
+		AU_Socket client = au_socket_connection_new(buffer, 8888);
+		printf("Enter a string:\n");
+		fgets(buffer, 512, stdin);
+		au_socket_udp_send(client, buffer, 512);
+		printf("Message sent: %s\n", buffer);
+	}
+	return 0;
+}
