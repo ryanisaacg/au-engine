d95d88655e38d9cfe7e59084d2ded7acef00bd0b
Ryan Goldstein <ryanisaacg@gmail.com>
2017-07-05 22:57:51 -0400

    Switched back to TCP in the example

--- example/net-example.c
+++ example/net-example.c
@@ -10,8 +10,8 @@ int main() {
 	//Run as server or client
 	if(buffer[0] == 'Y') {
 		AU_Socket server = au_socket_server_new(8888);
-		//AU_Socket client = au_socket_server_accept(server);
-		au_socket_udp_recv(server, buffer, 512);
+		AU_Socket client = au_socket_server_accept(server);
+		au_socket_tcp_recv(client, buffer, 512);
 		printf("Message received: %s\n", buffer);
 	} else {
 		printf("Enter the address of the server to connect to:\n");
@@ -19,7 +19,7 @@ int main() {
 		AU_Socket client = au_socket_connection_new(buffer, 8888);
 		printf("Enter a string:\n");
 		fgets(buffer, 512, stdin);
-		au_socket_udp_send(client, buffer, 512);
+		au_socket_tcp_send(client, buffer, 512);
 		printf("Message sent: %s\n", buffer);
 	}
 	return 0;
