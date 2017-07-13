25096e6995f29f56189dcb2e18c905466bc6328f
Ryan Goldstein <ryanisaacg@gmail.com>
2017-07-12 23:06:41 -0400

    Added a rendered shape example

--- example/example.c
+++ example/example.c
@@ -9,6 +9,7 @@ int main()
 	au_set_viewport(eng, au_viewport_new(AU_VIEWPORT_LETTERBOX, 16.0f / 9));
 	eng->camera.height = 450;
 	float x = 0, y = 0;
+	AU_Vector polygon[] = {{50, 50}, {50, 100}, {100, 100}, {100, 50}};
 	while(eng->should_continue) {
 		au_begin(eng, AU_BLACK);
 		if(eng->current_keys[SDL_SCANCODE_UP])
@@ -24,8 +25,9 @@ int main()
 			y = eng->mouse.y;
 		}
 		AU_TextureRegion region = au_tex_region(img);
+		au_draw_shape(eng, AU_BLUE, polygon, 4);
 		au_draw_texture_ex(eng, region, AU_WHITE, 0, 0, 32, 32, 0, 0, 0, 1, 1, false, false, 0.5f);
-		au_draw_texture_ex(eng, region, AU_GREEN, x, y, 32, 32, 0, 0, 0, 1, 1, false, false, 0);
+//		au_draw_texture_ex(eng, region, AU_GREEN, x, y, 32, 32, 0, 0, 0, 1, 1, false, false, 0);
 		au_draw_string(eng, font, "Hello there! test string", 0, 100);
 		au_end(eng);
 	}
