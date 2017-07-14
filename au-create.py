4f4019d49ccfbf80b73d3e4e35c27a41d597209b
Ryan Goldstein <ryanisaacg@gmail.com>
2017-07-14 18:26:51 -0400

    Fixed various bugs in the project create script

--- au-create.py
+++ au-create.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-if sys.argv != 2:
-    print("Pass a name for a project which will be created in a folder of the same name")
-else:
-    main()
 def main():
     name = sys.argv[1]
     cmake_contents='''cmake_minimum_required(VERSION 2.4.0)
@@ -21,16 +17,37 @@ include_directories(${au-engine-include})
 
 add_executable(game ${SOURCES})
 target_link_libraries(game ${au-engine-libs})
-configure_file(assets assets COPYONLY)
+file(COPY assets DESTINATION assets)
 '''
     os.system('mkdir ' + name)
     os.chdir(name)
+    with open('CMakeLists.txt', 'w') as f:
+        f.write(cmake_contents)
+    main_contents='''#include "au.h"
+int main() {
+    AU_Engine* eng = au_init("''' + name + '''", 800, 600);
+    while(eng->should_continue) {
+        au_begin(eng, AU_BLACK);
+        au_end(eng);
+    }
+    au_quit(eng);
+    return 0;
+}'''
+    with open('main.c', 'w') as f:
+        f.write(main_contents)
+    with open('.gitignore', 'w') as f:
+        f.write('build/')
     os.system('git init')
     os.system('git submodule add https://github.com/ryanisaacg/au-engine')
     os.system('mkdir build')
+    os.system('mkdir assets')
     os.chdir('build')
     if os.name == 'nt':
         os.system('cmake -G "MinGW Makefiles" ..')
     else:
         os.system('cmake ..')
     print('Created project structure for an AU project')
+if len(sys.argv) != 2:
+    print("Pass a name for a project which will be created in a folder of the same name")
+else:
+    main()
